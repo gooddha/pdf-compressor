3da771e181c335ca1562ed5e324e51328107fc34
Andrey Adamovich <gooddha@ya.ru>
2019-05-21 17:02:42 +0300

    first version

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const fs = require('fs');
 const chokidar = require('chokidar');
 const cmd = require('node-cmd');
 
 chokidar.watch('.', { ignored: /(^|[\/\\])\../ }).on('all', (event, path) => {
-    if (path.includes('pdf')) {
+    // console.log(path);
+    if (path.toLowerCase().includes('pdf')) {
         console.log('Added PDF File: ' + path);
-        cmd.run('gswin64c -dBATCH -dNOPAUSE -dSAFER -sDEVICE=jpeg -dJPEGQ=75 -r300 -sOutputFile=jpg/%03d.jpg ' + path);
+        const dir = path.split('.')[0];
+        console.log(dir);
+
+        fs.mkdirSync('jpg\\' + dir);
+        cmd.get('gswin64c -dBATCH -dNOPAUSE -dSAFER -sDEVICE=jpeg -dJPEGQ=75 -r300 -sOutputFile=' +'"' + 'jpg\\'
+            + dir + '\\%03d.jpg' + '" ' + '"' + path + '"', function(error, data){
+            console.log(error);
+            console.log(data);
+        });
     }
 });
