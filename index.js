be00ef19857a5e824be9f9fa9afd9d67bff795c7
Andrey Adamovich <gooddha@ya.ru>
2019-05-22 16:55:07 +0300

    Add compression and resize step

--- index.js
+++ index.js
@@ -6,19 +6,34 @@ chokidar.watch('.', { ignored: /(^|[\/\\])\../ }).on('add', (path) => {
 
     if (path.toLowerCase().includes('pdf')) {
         const dir = path.split('.').slice(0, -1).join('.');        
-        console.log(dir);
         
         if (!fs.existsSync('jpg\\' + dir)) {
             fs.mkdirSync('jpg\\' + dir);
             console.log(`Getting ${path}`)
+                        
             cmd.get(
-                'gswin32c -dBATCH -dNOPAUSE -dSAFER -sDEVICE=jpeg -dJPEGQ=75 -r300 -sOutputFile="' 
-                + `jpg\\${dir}\\%03d.jpg" ` + `"${path}"`, (error, data) => {
-                if (error) console.log(error);
-                console.log(data);
-                console.log('Converting done');
-                // nconvert -out jpeg -o process\#.jpg -resize 50% 50%  -dpi 150 *.jpg
-            });
+                `gswin32c -dBATCH -dNOPAUSE -dSAFER -sDEVICE=jpeg -dJPEGQ=75 -r300 -sOutputFile="jpg\\${dir}\\%03d.jpg" "${path}"`, (error, data) => {
+                    if (error) console.log(error);
+                    // console.log(data);
+                    console.log('Converting pdf to jpg done');    
+                    
+                    console.log('Getting started jpeg resize and compression');
+                    const resultDir = dir + '\\compressed';
+                    const srcDir = `jpg\\${dir}`;
+
+                    cmd.get(
+                        `nconvert -out jpeg -o "jpg\\${resultDir}\\#.jpg" -resize 50% 50%  -dpi 150 "${srcDir}\\*.jpg"`, (error, data) => {
+                            if (error) console.log(error);
+                            // console.log(data);
+                            console.log('Jpeg resize and compression done');
+
+                        }
+                    );            
+                }
+            );
+
+            
+            
         }
         
     }            
