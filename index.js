6ef93e96a7977c6336e121a5c487a87b6366f7cb
Andrey Adamovich <gooddha@ya.ru>
2019-05-23 13:55:55 +0300

    simplify directory structure, add some checks for directories existing

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ const fs = require('fs');
 const chokidar = require('chokidar');
 const cmd = require('node-cmd');
 
+//watch for new files in root directory
 chokidar.watch('.', { ignored: /(^|[\/\\])\../, depth: 0 }).on('add', (path) => {
 
+    if (!fs.existsSync('compressed')) {
+        fs.mkdirSync('compressed');
+    }
+
+    if (!fs.existsSync('compressed\\jpg')) {
+        fs.mkdirSync('compressed\\jpg');
+    }
+
+    //if added a pdf file 
     if (path.toLowerCase().includes('pdf')) {
-        const dir = path.split('.').slice(0, -1).join('.');        
+        const jpgDir = 'compressed\\jpg\\' + path.split('.').slice(0, -1).join('.');        
         
-        if (!fs.existsSync('jpg\\' + dir)) {
-            fs.mkdirSync('jpg\\' + dir);
+        if (!fs.existsSync(jpgDir)) {
+            fs.mkdirSync(jpgDir);
             console.log(`Getting ${path}`)
             
             //get jpegs from pdf
-            cmd.get(
-                `gswin32c -dBATCH -dNOPAUSE -dSAFER -sDEVICE=jpeg -dJPEGQ=75 -r300 -sOutputFile="jpg\\${dir}\\%03d.jpg" "${path}"`, (error, data) => {
+            cmd.get(`gswin32c -dBATCH -dNOPAUSE -dSAFER -sDEVICE=jpeg -dJPEGQ=75 -r300 -sOutputFile="${jpgDir}\\%03d.jpg" "${path}"`, (error, data) => {
+                if (error) console.log(error);
+                
+                //get compressed pdf from jpegs
+                cmd.get(`magick "${jpgDir}\\*.jpg" -resize 50%  -density 150 -compress JPEG -quality 30 "compressed\\${path}"`, (error, data) => {
                     if (error) console.log(error);
-                    
-                    const resultDir = `jpg\\${dir}\\compressed`;
-                    const srcDir = `jpg\\${dir}`;
-                    fs.mkdirSync(resultDir);
-
-                    //get compressed pdf from jpegs
-                    cmd.get(
-                        `magick "${srcDir}\\*.jpg" -resize 50%  -density 150 -compress JPEG -quality 30 "compressed\\${path}"`, (error, data) => {
-                            if (error) console.log(error);
-                            console.log(`${path} is compressed`);
-                        }
-                    );            
-                }
-            );         
+                    console.log(`${path} is compressed`);
+                });            
+            });         
         }
     }            
 });
