6f2d9aecdf10554e815afc5430aef0e98f37a024
Andrey Adamovich <gooddha@ya.ru>
2019-05-23 13:36:19 +0300

    merge resizing+compressing and jpeg 2 pdf steps

--- index.js
+++ index.js
@@ -10,37 +10,25 @@ chokidar.watch('.', { ignored: /(^|[\/\\])\../, depth: 0 }).on('add', (path) => 
         if (!fs.existsSync('jpg\\' + dir)) {
             fs.mkdirSync('jpg\\' + dir);
             console.log(`Getting ${path}`)
-                        
+            
+            //get jpegs from pdf
             cmd.get(
                 `gswin32c -dBATCH -dNOPAUSE -dSAFER -sDEVICE=jpeg -dJPEGQ=75 -r300 -sOutputFile="jpg\\${dir}\\%03d.jpg" "${path}"`, (error, data) => {
                     if (error) console.log(error);
-                    // console.log(data);
-                    console.log('Converting pdf to jpg done');    
                     
-                    console.log('Getting started jpeg resize and compression');
                     const resultDir = `jpg\\${dir}\\compressed`;
                     const srcDir = `jpg\\${dir}`;
                     fs.mkdirSync(resultDir);
 
+                    //get compressed pdf from jpegs
                     cmd.get(
-                        // `nconvert -out jpeg -o "${resultDir}\\#.jpg" -resize 50% 50%  -dpi 150 "${srcDir}\\*.jpg"`, (error, data) => {
-                        `magick "${srcDir}\\*.jpg" -resize 50%  -density 150 "${resultDir}\\%03d.jpg"`, (error, data) => {
+                        `magick "${srcDir}\\*.jpg" -resize 50%  -density 150 -compress JPEG -quality 30 "compressed\\${path}"`, (error, data) => {
                             if (error) console.log(error);
-                            // console.log(data);
-                            console.log('Jpeg resize and compression done');
-
-                            cmd.get(
-                                `magick "${resultDir}\\*.jpg" -compress JPEG -quality 30 "compressed\\${path}"`, (error, data) => {
-                                    if (error) console.log(error);
-                                    else console.log(`${path} compressed`);
-                                }
-                            )
+                            console.log(`${path} is compressed`);
                         }
                     );            
                 }
-            );           
-            
+            );         
         }
-        
     }            
 });
