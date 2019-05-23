1dd128cade24c860589af8f96d56a72ddca24370
Andrey Adamovich <gooddha@ya.ru>
2019-05-23 13:27:47 +0300

    replace nconvert jpeg resizing to imagemagick

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const chokidar = require('chokidar');
 const cmd = require('node-cmd');
-const { exec } = require('child_process');
 
 chokidar.watch('.', { ignored: /(^|[\/\\])\../, depth: 0 }).on('add', (path) => {
 
@@ -19,17 +18,19 @@ chokidar.watch('.', { ignored: /(^|[\/\\])\../, depth: 0 }).on('add', (path) => 
                     console.log('Converting pdf to jpg done');    
                     
                     console.log('Getting started jpeg resize and compression');
-                    const resultDir = dir + '\\compressed';
+                    const resultDir = `jpg\\${dir}\\compressed`;
                     const srcDir = `jpg\\${dir}`;
+                    fs.mkdirSync(resultDir);
 
                     cmd.get(
-                        `nconvert -out jpeg -o "jpg\\${resultDir}\\#.jpg" -resize 50% 50%  -dpi 150 "${srcDir}\\*.jpg"`, (error, data) => {
+                        // `nconvert -out jpeg -o "${resultDir}\\#.jpg" -resize 50% 50%  -dpi 150 "${srcDir}\\*.jpg"`, (error, data) => {
+                        `magick "${srcDir}\\*.jpg" -resize 50%  -density 150 "${resultDir}\\%03d.jpg"`, (error, data) => {
                             if (error) console.log(error);
                             // console.log(data);
                             console.log('Jpeg resize and compression done');
 
                             cmd.get(
-                                `magick "jpg\\${resultDir}\\*.jpg" -compress JPEG -quality 30 "compressed\\${path}"`, (error, data) => {
+                                `magick "${resultDir}\\*.jpg" -compress JPEG -quality 30 "compressed\\${path}"`, (error, data) => {
                                     if (error) console.log(error);
                                     else console.log(`${path} compressed`);
                                 }
