e9b05ba83be4b0904cb6d06f54aaaba2c33ac77a
Andrey Adamovich <gooddha@ya.ru>
2019-11-12 16:48:47 +0300

    refactor callbacks style to promises

--- index.js
+++ index.js
@@ -4,49 +4,60 @@ const cmd = require('node-cmd');
 const gs = 'assets\\gs\\bin\\gswin32c';
 const dirs = ['compressed', 'compressed\\jpg'];
 
-
-
-//watch for new files in the root directory
-chokidar.watch('.', { ignored: /(^|[\/\\])\../, depth: 0 }).on('add', (newFile) => {
-
-    initializeDirs(dirs);
-
-    //if a pdf file was added  
-    if (newFile.toLowerCase().includes('pdf')) {
-        const pdf = newFile;
-        const jpgDir = 'compressed\\jpg\\' + pdf.split('.').slice(0, -1).join('.');        
-        
-        if (!fs.existsSync(jpgDir)) {
-            fs.mkdirSync(jpgDir);
-            print(`Getting ${pdf}`)
-            
-            //get jpegs from pdf
-            const gsOptions = '-dBATCH -dNOPAUSE -dSAFER -sDEVICE=jpeg -dJPEGQ=75 -r300';
-            cmd.get(`${gs} ${gsOptions} -sOutputFile="${jpgDir}\\%03d.jpg" "${pdf}"`, (error, data) => {                
-                if (error) console.log(error);
-                print(`${pdf} pages is splitted by jpegs, start ${pdf} compression`);
-                
-                //get compressed pdf from jpegs
-                const magickOptions = '-resize 50%  -density 150 -strip -compress JPEG -quality 30 ';
-                cmd.get(`magick "${jpgDir}\\*.jpg" ${magickOptions} "compressed\\${pdf}"`, (error, data) => {
-
-                    if (error) console.log(error);
-                    print(`${pdf} is compressed`);
-
-                });            
-            });         
-        }
-    }            
+dirs.forEach(dir => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
 });
 
-function initializeDirs(dirs) {
-    dirs.forEach(dir => {
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
-        }
-    });
+print(`PDF compressor is started and watching for PDF files...`);
+chokidar.watch('.', { ignored: /(^|[\/\\])\../, depth: 0 }).on('add', compress);
 
+function compress(newFile) {
+    const pdf = newFile;
+    const jpgDir = 'compressed\\jpg\\' + pdf.split('.').slice(0, -1).join('.');    
+    
+    if (newFile.toLowerCase().includes('pdf')) {
+
+        if (!fs.existsSync(jpgDir)) {
+            fs.mkdirSync(jpgDir);
+            jpgSplit().then(pdfCompress).catch(error => print(error));
+        };
+        
+    }    
+    
+    function jpgSplit() {
+        print(`Getting ${pdf}`);
+        const gsOptions = '-dBATCH -dNOPAUSE -dSAFER -sDEVICE=jpeg -dJPEGQ=75 -r300';
+        const gsCMD = `${gs} ${gsOptions} -sOutputFile="${jpgDir}\\%03d.jpg" "${pdf}"`;
+        
+        return new Promise((resolve, reject) => {
+            cmd.get(gsCMD, (error, data) => {
+                if (error) reject(error);
+                print(`${pdf} pages is splitted by jpegs, start ${pdf} compression`);
+                resolve();
+            })
+        }) 
+    }
+    
+    
+    function pdfCompress() {
+        const magickOptions = '-resize 50%  -density 150 -strip -compress JPEG -quality 30 ';
+        const magikCMD = `magick "${jpgDir}\\*.jpg" ${magickOptions} "compressed\\${pdf}"`;
+        
+        cmd.get(magikCMD, (error) => {
+            if (error) reject(error);
+            print(`${pdf} is compressed`);
+        });            
+    }
+
+};
+
+
+function print(message) {
+    return console.log(`${printTime()}${message}`)
 }
+
 
 function printTime() {
     const now = new Date();
@@ -59,8 +70,4 @@ function printTime() {
     function addLeadingZero(number) {
         return number < 10 ? `0${number}` : number;
     }
-}
-
-function print(message) {
-    return console.log(`${printTime()}${message}`)
 }
